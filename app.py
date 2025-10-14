011bee841de1d7366589da117085a3994945295b
Andrew-Negrut <andrewnegrut@gmail.com>
2025-10-14 16:12:56 -0500

    update path

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from app.gradio_app import demo
+import os
+import sys
+
+ROOT = os.path.dirname(__file__)
+APP_DIR = os.path.join(ROOT, "app")
+if APP_DIR not in sys.path:
+    sys.path.insert(0, APP_DIR)
+
+from gradio_app import demo
 
 if __name__ == "__main__":
-    demo.queue(concurrency_count=2).launch()
+    demo.launch(server_name="0.0.0.0", server_port=int(os.getenv("PORT", "7860")), share=False)
